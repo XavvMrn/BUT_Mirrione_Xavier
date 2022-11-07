83da9f299967eb5914ab1e71581d27d42477120d
IUT GEII Toulon Table 5 <touloniut@gmail.com>
2022-11-07 08:16:39 +0100

    RobotInterfaceAvantVacances

---
+++ RobotInterface_XM/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RobotInterface_XM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool toggle = false;
+        private void ButtonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            if (toggle)
+            {
+                ButtonEnvoyer.Background = Brushes.Beige;
+            }
+            else
+            {
+                ButtonEnvoyer.Background = Brushes.RoyalBlue;
+            }
+            toggle = !toggle;
+            SendMessage();
+        }
+
+        private void SendMessage()
+        {
+            // Envoie msg Emiss° à Recept°
+            string selectxt = textBoxEnvoi.Text;
+            textBoxReception.Text = "Recu :" + selectxt;
+            textBoxEnvoi.Text = "";
+        }
+
+        private void textBoxEnvoi_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {                
+                SendMessage();
+            }
+        }
+    }
+}
